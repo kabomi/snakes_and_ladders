15790ed8962c1d80a8575ccb98d5c25d6a0cc0c4
Imobach Martin <kabomi@gmail.com>
2014-08-29 18:51:30 +0200

    red

--- src/client/spec/app_spec.js
+++ src/client/spec/app_spec.js
@@ -5,9 +5,22 @@
     
     describe("Snake and ladders", function (){
         describe("Player", function(){
+            var player;
+            beforeEach(function(){
+                player = new app.Player(5);
+            });
             it("defines its position", function(){
-                 var player = new app.Player(5);
                  expect(player.position).toBe(5);
+            });
+            it("moves to another position from its current position", function(){
+                player.move(5);
+                expect(player.position).toBe(10);
+                player.move(10);
+                expect(player.position).toBe(20);
+            });
+            it("moves backwards from its current position", function(){
+                player.moveBackwards(5);
+                expect(player.position).toBe(0);
             });
         });
         
