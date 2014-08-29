fa65fc43cc76156b6cd67d6a879fdac1edfb7d61
Imobach Martin <kabomi@gmail.com>
2014-08-29 18:51:58 +0200

    green

--- src/client/app.js
+++ src/client/app.js
@@ -11,6 +11,20 @@ var app = app || {};
         
         return self;
     }
+    function Player(position){
+        var self ={
+            position: position,
+            move: function(newPosition){
+                self.position = self.position + newPosition;
+            },
+            moveBackwards: function(numPositions){
+                self.position = self.position - numPositions;
+            }
+        };
 
+        return self;
+    }
+
+    app.Player = Player;
     app.init = init; 
 })();
