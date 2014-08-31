b1a6bf93d838d38e5ac691caab6453d0621d9af6
Imobach Martin <kabomi@gmail.com>
2014-08-31 22:06:16 +0200

    green

--- src/client/app.js
+++ src/client/app.js
@@ -18,14 +18,15 @@ var app = app || {};
                 }
                 player.position += player.nextMove;
 
-                player.position = board.getSnakeEndFrom(player.position);
-                
-                player.position = board.getLadderEndFrom(player.position);
-                
                 if (player.position === board.lastField){
                     self.winner = player;
                     self.finished = true;
                 }
+                
+                player.position = board.getSnakeEndFrom(player.position);
+                
+                player.position = board.getLadderEndFrom(player.position);
+                
                 return true;
             }
         };
